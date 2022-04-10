27d04ae471c5a3fcf9562ea2556746324d6efeb0
CanerCeylan <canerceylan644@hotmail.com>
2022-04-10 18:45:18 +0300

    bölme işlemindeki 0a bölme hatası giderildi

--- HesapMakinesi.c
+++ HesapMakinesi.c
@@ -27,8 +27,14 @@ int main(){
 			scanf("%lf",&sayi1);
 			printf("Ikinci sayiyi giriniz: ");
 			scanf("%lf",&sayi2);
-			sonuc=sayi1/sayi2;
-			printf("Sonuc = %lf\n",sonuc);
+			if(sayi2!=0){
+				sonuc=sayi1/sayi2;
+				printf("Sonuc = %lf\n",sonuc);
+			}
+			else{
+				printf("Bolen 0 olamaz!\n");
+			}
+
 			printf("------------------------------\n");
 		}
 
