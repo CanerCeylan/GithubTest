169128525a84a195e6042892fb9907386310bc75
CanerCeylan <canerceylan644@hotmail.com>
2022-04-10 18:36:01 +0300

    bölme işlemi yazıldı

--- HesapMakinesi.c
+++ HesapMakinesi.c
@@ -5,16 +5,32 @@ int main(){
 
 	while(1){
 		double sayi1,sayi2,sonuc;
+		char islem;
 
 		printf("___Hesap Makinesi___\n");
-		printf("Bu hesap makinesi sadece toplama yapar!\n");
-		printf("Birinci sayiyi giriniz: ");
-		scanf("%lf",&sayi1);
-		printf("Ikinci sayiyi giriniz: ");
-		scanf("%lf",&sayi2);
-		sonuc=sayi1+sayi2;
-		printf("Sonuc = %lf\n",sonuc);
-		printf("------------------------------\n");
+		printf("Yapmak istediginiz islemi seciniz:\n");
+		printf("Toplama icin -> +\n");
+		printf("Bolme icin -> /\n");
+		scanf("%c",&islem);
+
+		if(islem=='+'){
+			printf("Birinci sayiyi giriniz: ");
+			scanf("%lf",&sayi1);
+			printf("Ikinci sayiyi giriniz: ");
+			scanf("%lf",&sayi2);
+			sonuc=sayi1+sayi2;
+			printf("Sonuc = %lf\n",sonuc);
+			printf("------------------------------\n");
+		}
+		else if(islem=='/'){
+			printf("Birinci sayiyi giriniz: ");
+			scanf("%lf",&sayi1);
+			printf("Ikinci sayiyi giriniz: ");
+			scanf("%lf",&sayi2);
+			sonuc=sayi1/sayi2;
+			printf("Sonuc = %lf\n",sonuc);
+			printf("------------------------------\n");
+		}
 
 	}
 
